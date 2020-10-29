dcdf49ea183236f6f07544c5d5542b659edf4857
B20-Project <dilyar.aji@hotmail.com>
2020-10-29 01:22:42 +0100

    cleanup

--- src/test/java/com/CRM24/step_definitions/base_navigation/iMessageStepDefinition.java
+++ src/test/java/com/CRM24/step_definitions/base_navigation/iMessageStepDefinition.java
@@ -1,9 +1,9 @@
 package com.CRM24.step_definitions.base_navigation;
 
-import com.CRM24.pages.base_navigation_page.Messenger;
+import com.CRM24.pages.base_navigation_page.ChatBar;
 
 public class iMessageStepDefinition {
 
-    Messenger msg = new Messenger();
+    ChatBar msg = new ChatBar();
 
 }
